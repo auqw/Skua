dfadbad043f15ac7b4775bd8ac1f31700a7188b4
blackarrow123 <dev@jcsawyer.me>
2026-02-20 19:21:47 +0000

    extract shared theme panels

---
+++ Skua.Shared.Avalonia/Controls/Theming/ThemeLibraryPanel.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Skua.Shared.Avalonia.Controls.Theming;
+
+public partial class ThemeLibraryPanel : UserControl
+{
+    public ThemeLibraryPanel()
+    {
+        InitializeComponent();
+    }
+}
