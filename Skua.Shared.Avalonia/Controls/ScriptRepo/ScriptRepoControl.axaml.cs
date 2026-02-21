667995a6cb04af162c053bb9e1862da43cde8b66
blackarrow123 <dev@jcsawyer.me>
2026-02-21 13:35:54 +0000

    shared script repo parity and manager dialog polish

---
+++ Skua.Shared.Avalonia/Controls/ScriptRepo/ScriptRepoControl.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Skua.Shared.Avalonia.Controls.ScriptRepo;
+
+public partial class ScriptRepoControl : UserControl
+{
+    public ScriptRepoControl()
+    {
+        InitializeComponent();
+    }
+}
